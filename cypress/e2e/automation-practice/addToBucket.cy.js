f06c539e0a03b7b493d18b0e8ee1d950258aabfb
Adam Kowalewski <akowalewski@deloittece.com>
2022-08-04 15:49:47 +0200

    Second and Third tasks

--- cypress/e2e/automation-practice/addToBucket.cy.js
+++ cypress/e2e/automation-practice/addToBucket.cy.js
@@ -5,28 +5,40 @@ import {MainPage} from "../page_objects/main-page"
 
 context('e-shop go to', () => {
 
-    var value;
+    var productsPrice;
     
     beforeEach(() => {    
     MainPage.openAutomationPracticePage();
 
-    cy.get(".price.product-price").first().then(($span) =>{
-      value = $span.text();
-      
-    })
+    cy.get(".price.product-price").first().invoke('text').as('productPrice');
   })
 
   describe('test',() => {
         
-        it('should open Sign in', () => {
+        it('add to bucket', () => {
 
+
+          cy.get('@productPrice').then((productPrice) => {
+            
+            let productPriceValue = parseFloat(productPrice.replace('$', ''));
+            
+            productsPrice = (2 * productPriceValue);
+            
+            
+            
+          })
           
-          //alert(value);
              
             MainPage.clickAddCard();
-            
+            cy.wait(4000);
             MainPage.clickContinueShopping();
             MainPage.clickAddCard();
+            cy.wait(7000);
+            cy.get(".ajax_block_products_total").invoke('text').then((total) => {
+              let totalValue = parseFloat(total.replace('$', ''));
+              
+              expect(totalValue).to.equal(parseFloat(productsPrice))
+            })
            })
     
 
